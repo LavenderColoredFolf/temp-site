93de67406e4f05a2c9358a023a584525b3f60322
Alexander Hyman <alexander.hyman05@gmail.com>
2022-11-26 19:05:43 -0800

    ESLint

--- next.config.js
+++ next.config.js
@@ -8,6 +8,11 @@ const withMDX = require('@next/mdx')({
 
 /** @type {import('next').NextConfig} */
 module.exports = withMDX({
+	eslint: {
+		// Warning: This allows production builds to successfully complete even if
+		// your project has ESLint errors.
+		ignoreDuringBuilds: true,
+	  },
 	reactStrictMode: true,
 	pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx'],
 	images: {
